a9dd314e62a5fd1a4237a94f156113ee13da2bed
ryomahan <ryomahan1996@gmail.com>
2020-06-22 15:13:20 +0800

    feat: create project

---
+++ wsgiref/__init__.py
@@ -0,0 +1,23 @@
+"""wsgiref -- a WSGI (PEP 3333) Reference提及，参考 Library
+
+Current Contents: 当前目录
+
+* util -- Miscellaneous 各种各样的，混杂的 useful functions and wrappers
+
+* headers -- Manage response headers
+
+* handlers -- base classes for server/gateway implementations 实现
+
+* simple_server -- a simple BaseHTTPServer that supports提供 WSGI
+
+* validate -- validation wrapper that sits between an app and a server
+  to detect 检测 errors in either
+
+To-Do:
+
+* cgi_gateway -- Run WSGI apps under CGI (pending a deployment standard)
+
+* cgi_wrapper -- Run CGI apps under WSGI
+
+* router -- a simple middleware component that handles URL traversal
+"""
